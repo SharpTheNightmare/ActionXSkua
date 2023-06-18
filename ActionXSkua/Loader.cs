8f08f56e28ef96b7b42ce81c1a946d74817534ab
Purple <koen.t04@gmail.com>
2023-06-18 08:55:04 -0500

    Init

---
+++ ActionXSkua/Loader.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Skua.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace ExamplePlugin
+{
+    public partial class Loader : ISkuaPlugin
+    {
+        public static IScriptInterface Bot => IScriptInterface.Instance;
+
+        public string Name => "Example Plugin";
+        public string Author => "SharpTheNightmare";
+        public string Description => "Gives an example of ISkuaPlugin";
+
+        public List<IOption>? Options { get; } = new();
+
+        public IPluginHelper? Helper { get; private set; }
+
+        public void Load(IServiceProvider provider, IPluginHelper helper)
+        {
+            Helper = helper;
+
+            helper.AddMenuButton(Name, () =>
+            {
+                MainWindow.Instance.Show();
+                MainWindow.Instance.BringIntoView();
+                MainWindow.Instance.Activate();
+            });
+
+            Bot?.Log($"{Name} Loaded.");
+        }
+
+        public void Unload()
+        {
+            Bot?.Log($"{Name} Unloaded.");
+            Helper?.RemoveMenuButton(Name);
+        }
+    }
+}
